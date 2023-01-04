e74ec00230b59e6e17bf3720338748de30b7adf6
Chloe Lim <hylim9@gmail.com>
2023-01-04 16:19:17 +0900

    add css

--- react-beginners/src/routes/Detail.js
+++ react-beginners/src/routes/Detail.js
@@ -5,7 +5,7 @@ import MovieDetail from "../components/MovieDetail";
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState({});
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
